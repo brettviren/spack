3350133a27969292d4f815f7c7ec1a43d158228b
Brett Viren <bv@bnl.gov>
2016-02-17 18:56:34 -0500

    Initial Python packaging of Spack itself.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'spack',
+    version = '0.0',
+    license = 'GPL',
+    url = 'https://github.com/LLNL/spack',
+    description = 'A flexible package manager designed to support multiple versions, configurations, platforms, and compilers.',
+    author = 'LLNL',
+    author_email = 'spack@googlegroups.com',
+    maintainer = 'Brett Viren',
+    maintainer_email = 'brett.viren@gmail.com',
+
+    package_dir = {'spack':'lib/spack/spack'},
+
+    packages = find_packages("lib/spack",exclude=('external*',)),
+
+    install_requires = [l for l in open("requirements.txt").readlines() if l.strip()],
+
+    entry_points = dict(
+        console_scripts = [
+            'spack = spack.cli.main',
+        ]
+    ),
+
+
+)
+
