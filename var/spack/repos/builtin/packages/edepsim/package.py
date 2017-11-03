7226fe3abd73995da3966e8e17e3b084a49bcac3
Brett Viren <brett.viren@gmail.com>
2017-11-03 16:08:58 -0400

    Add edep-sim.

---
+++ var/spack/repos/builtin/packages/edepsim/package.py
@@ -0,0 +1,12 @@
+from spack import *
+
+class Edepsim(CMakePackage):
+    """An energy deposition simulation based on Geant4"""
+    homepage = "https://github.com/ClarkMcGrew/edep-sim/"
+    url = "https://github.com/ClarkMcGrew/edep-sim/archive/2.0.1.tar.gz"
+
+    version('2.0.1', '88afc6f7570f19efd2094fce00585c00')
+
+    depends_on("geant4~qt")
+    depends_on("root")
+    depends_on("cmake", type='build')
